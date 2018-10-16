c993e1c86b3ae59323b754cd42df09e2da882081
Aaron Wong <aaronwong13@gmail.com>
2018-10-16 14:24:41 -0700

    trick

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var ip = 127.0.0.1;
 var server = http.createServer(function (response, request) {
 	if (request.method === 'GET') {
 		response.writeHead(200, 'OK', {`Content-Type`: 'text/plain'});
-		var data = 'Hello, World!';
+		var data = new String('Hello, World!');
 		response.end(data);
 	} else {
 		response.end(404, 'Sorry, I only accept GET requests!');
